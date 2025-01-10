30c674fdc5116087607588fc51bb6376be1f5b3a
mkeloo <mkeloo@ufl.edu>
2025-01-10 02:30:37 -0500

    Refactor Supabase client import to use browser client for device code fetching

--- app/actions/getStoreDeviceCodes.ts
+++ app/actions/getStoreDeviceCodes.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 
 // Fetch client device code
 export async function getStoreClientCode() {
